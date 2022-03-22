fdd25f24af41f7247579ac794882b3381b780e35
qmamorup <96193979+qmamorup@users.noreply.github.com>
2022-03-22 19:25:52 +0300

    Add project files.

---
+++ std_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdint.h>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+	std::string first = "n1246534550dewewq";
+	std::cout << first.length() << "\n";
+
+	std::string str1 = "n1246534550dewewq";
+	std::cout << str1[0] << std::endl;
+
+	std::string str2 = "n1246534550dewewq";
+	std::cout << str1[16] << std::endl;
+	
+	std::cin;
+	return 0;
+}
+
+
+
